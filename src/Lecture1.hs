8e692290343688ba759d6a29069e5a3279d13190
Marcin Jaskolski <marcin.jaskolski@gmail.com>
2022-01-16 23:38:51 +0100

    tests pass

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -35,6 +35,7 @@ module Lecture1
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+makeSnippet :: Int -> [Char] -> [Char]
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
@@ -50,7 +51,8 @@ Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: Int -> Int -> Int
+sumOfSquares x y = (x * x) + (y * y)
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -63,7 +65,10 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n
+    | n < 0 = mod (-n) 10
+    | otherwise = mod n 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -77,7 +82,11 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+minmax :: Int -> Int -> Int -> Int
+minmax x y z =
+    let max3 = max (max x y) z
+        min3 = min (min x y) z
+    in max3 - min3
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -94,7 +103,11 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+subString :: Int -> Int -> [Char] -> [Char]
+subString start end str
+    | start < 0 = subString 0 end str
+    | end < 0   = []
+    | otherwise = reverse (take ((end - start) + 1) (reverse (take (end+1) str)))
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -104,7 +117,28 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
+
+strSum :: [Char] -> Int
+strSum str1 = sumListAccu (splitToInts str1) 0
+    where
+      splitAccu :: [Char] -> [Char] -> [[Char]] -> [[Char]]
+      splitAccu str strAccu strListAccu
+          | (null str) && (null strAccu) = strListAccu
+          | (null str) = strAccu : strListAccu
+          | (head str == ' ') && (null strAccu) = splitAccu (tail str) [] strListAccu
+          | (head str == ' ') = [strAccu] ++ (splitAccu (tail str) [] strListAccu)
+          | otherwise = splitAccu (tail str) (strAccu ++ [head str]) strListAccu
+    
+      split :: [Char] -> [[Char]]
+      split strList = splitAccu strList [] []
+
+      splitToInts :: [Char] -> [Int]
+      splitToInts list = map (\x -> read x :: Int) (split list)
+
+      sumListAccu:: [Int] -> Int -> Int
+      sumListAccu list accu
+          | null list = accu
+          | otherwise = sumListAccu (tail list) (head list) + accu
 
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
@@ -119,4 +153,14 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+
+lowerAndGreater :: Int -> [Int] -> [Char]
+lowerAndGreater n list1 = (show n) ++ " is greater than " ++ (show (fst nums)) ++ " elements and lower than " ++ (show (snd nums)) ++ " elements"
+    where
+        lg :: [Int] -> Int -> (Int, Int) -> (Int, Int)
+        lg list bound accu
+            | null list = accu
+            | (head list < bound) = lg (tail list) bound ((fst accu)+1, (snd accu))
+            | (head list > bound) = lg (tail list) bound ((fst accu), (snd accu)+1)
+            | otherwise = lg (tail list) bound ((fst accu), (snd accu))
+        nums = lg list1 n (0, 0)
