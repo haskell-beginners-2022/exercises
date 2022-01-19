d4edea96c79f59d7def5d699eeb6fecef6625ca3
Vladislav Dubovski <444NegaTiV@gmail.com>
2022-01-19 20:33:51 +0000

    merge

--- src/Lecture2.hs
+++ src/Lecture2.hs
@@ -196,7 +196,12 @@ verify that.
 [1,2,3,4,7]
 -}
 merge :: [Int] -> [Int] -> [Int]
-merge = error "TODO"
+merge [] [] = []
+merge [] y = y
+merge x [] = x
+merge (x : xs) (y : ys) = if x < y
+  then x : merge xs (y : ys)
+  else y : merge (x : xs) ys 
 
 {- | Implement the "Merge Sort" algorithm in Haskell. The @mergeSort@
 function takes a list of numbers and returns a new list containing the
