86c2794ea7dd62a7b85f0b36f81f20cf20d078e8
Olga Kabirova <85229400+olgakabirova@users.noreply.github.com>
2022-01-22 19:50:36 +0100

    Update src/Lecture1.hs
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -66,7 +66,7 @@ sumOfSquares x y = x*x + y*y
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
 lastDigit :: Int  ->  Int
-lastDigit n = mod (abs n) 10
+lastDigit n = abs n `mod` 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
