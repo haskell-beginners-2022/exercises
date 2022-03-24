c3fef984835fa2fc204f93919b98d1533a9f1cd8
a.vorobyev <a.vorobyev@oneliya.ru>
2022-03-24 11:13:11 +0300

    implemented solutions for lecture 1

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -20,6 +20,7 @@ It's absolutely okay if you feel that your implementations are not
 perfect. You can return to these exercises after future lectures and
 improve your solutions if you see any possible improvements.
 -}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Lecture1
     ( makeSnippet
@@ -39,6 +40,8 @@ module Lecture1
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+
+makeSnippet :: Int -> [Char] -> [Char]
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
@@ -54,7 +57,8 @@ Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: Int -> Int -> Int
+sumOfSquares x y = x*x + y*y
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -67,7 +71,8 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = mod (abs n) 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -81,7 +86,13 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+minmax :: Int -> Int -> Int -> Int
+minmax x y z =
+    let  l = min (min x y) z
+         h = max (max x y) z
+    in h - l
+
+
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -98,7 +109,16 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+subString :: Int -> Int -> String -> String
+subString start end str =
+    if start > 0
+    then
+        let 
+
+            s = start -1
+            e = end - 1
+        in subString s e (tail str)
+    else take (end + 1) str
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -108,7 +128,20 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
+strSum :: String -> Int
+strSum str =
+    let l = words str
+    in sumStrList l
+
+sumStrList :: [String] -> Int
+sumStrList l =
+    if null l 
+    then 0
+    else    
+        let
+            s = read (head l) :: Int
+        in s + sumStrList(tail l)
+
 
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
@@ -123,4 +156,37 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+
+lowerAndGreater :: Int -> [Int] -> String
+lowerAndGreater n list = 
+    let 
+        cnt = getGreaterLowerCnt n list
+    in show n ++ " is greater than " ++ show (head cnt) ++ " elements and lower than " ++ show (last cnt) ++ " elements"   
+
+getGreaterLowerCnt :: Int -> [Int] -> [Int]    
+getGreaterLowerCnt n list
+    | null list  = [0, 0] 
+    | headOrDefault list < n =
+        let 
+            cnt = [1, 0]
+        in  sumCnt cnt (getGreaterLowerCnt n (tail list))
+    | headOrDefault list > n =
+        let 
+            cnt = [0, 1]
+        in  sumCnt cnt (getGreaterLowerCnt n (tail list))
+    | otherwise = 
+        let 
+            cnt = [0, 0]
+        in  sumCnt cnt (getGreaterLowerCnt n (tail list))     
+
+sumCnt :: [Int] -> [Int] -> [Int]
+sumCnt l1 l2 = 
+    let 
+        h = head l1 + head l2
+        l = last l1 + last l2
+    in [h, l]    
+
+
+headOrDefault :: [Int] -> Int   
+headOrDefault list = 
+    if null list then 0 else head list
